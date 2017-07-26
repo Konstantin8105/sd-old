4bc93dc7c64b4709893f9d6bf4b27a115c0f50d4
Konstantin8105 <Konstantin8105@gmail.com>
2017-07-26 21:16:29 +0300

    refactoring

--- solver/finiteElement/utils.go
+++ solver/finiteElement/utils.go
@@ -5,6 +5,68 @@ import (
 	"github.com/Konstantin8105/GoLinAlg/matrix"
 )
 
+// GetStiffinerGlobalK - global matrix of stiffiner
+func GetStiffinerGlobalK(f FiniteElementer, degree *dof.DoF, info Information) (matrix.T64, []dof.AxeNumber) {
+	klocal := matrix.NewMatrix64bySize(4, 4)
+	f.GetStiffinerK(&klocal)
+
+	Tr := matrix.NewMatrix64bySize(4, 4)
+	f.GetCoordinateTransformation(&Tr)
+
+	kor := klocal.MultiplyTtKT(Tr)
+
+	axes := f.GetDoF(degree)
+
+	if info == WithoutZeroStiffiner {
+		RemoveZeros(&kor, &axes)
+	}
+
+	return kor, axes
+}
+
+/*
+// GetGlobalMass - global matrix of mass
+func GetGlobalMass(f FiniteElementer, degree *dof.DoF, info Information) (matrix.T64, []dof.AxeNumber) {
+	mlocal := matrix.NewMatrix64bySize(4, 4)
+	f.GetMassMr(&mlocal)
+
+	Tr := matrix.NewMatrix64bySize(4, 4)
+	f.GetCoordinateTransformation(&Tr)
+
+	mor := mlocal.MultiplyTtKT(Tr)
+
+	axes := f.GetDoF(degree)
+
+	if info == WithoutZeroStiffiner {
+		RemoveZeros(&mor, &axes)
+	}
+
+	return mor, axes
+}
+*/
+
+/*
+// GetGlobalPotential - global matrix of mass
+func GetGlobalPotential(f FiniteElementer, degree *dof.DoF, info Information) (linAlg.Matrix64, []dof.AxeNumber) {
+	mlocal := linAlg.NewMatrix64bySize(4, 4)
+	f.GetPotentialGr(&mlocal, -1)
+	panic("TODO")
+
+	Tr := linAlg.NewMatrix64bySize(4, 4)
+	f.GetCoordinateTransformation(&Tr)
+
+	mor := mlocal.MultiplyTtKT(Tr)
+
+	axes := f.GetDoF(degree)
+
+	if info == WithoutZeroStiffiner {
+		RemoveZeros(&mor, &axes)
+	}
+
+	return mor, axes
+}
+*/
+
 // RemoveZeros - remove columns, rows of matrix and columns of dof
 func RemoveZeros(matrix *matrix.T64, axes *[]dof.AxeNumber) {
 	var removePosition []int
