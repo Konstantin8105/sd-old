258e9577b09636a8e83b3585fa3dcf70ec36f4ed
Konstantin8105 <konstantin8105@gmail.com>
2017-07-31 16:00:32 +0300

    remove wrong comments

--- solver/model/group2dNodeForce.go
+++ solver/model/group2dNodeForce.go
@@ -13,9 +13,8 @@ type nodeForce2d struct {
 // Sorting
 type nodeForceByPoint []nodeForce2d
 
-func (a nodeForceByPoint) Len() int { return len(a) }
-
-//func (a nodeForceByPoint) Swap(i, j int)       { a[i], a[j] = a[j], a[i] }
-//func (a nodeForceByPoint) Less(i, j int) bool  { return a[i].pointIndex < a[j].pointIndex }
+func (a nodeForceByPoint) Len() int            { return len(a) }
+func (a nodeForceByPoint) Swap(i, j int)       { a[i], a[j] = a[j], a[i] }
+func (a nodeForceByPoint) Less(i, j int) bool  { return a[i].pointIndex < a[j].pointIndex }
 func (a nodeForceByPoint) Equal(i, j int) bool { return a[i].pointIndex == a[j].pointIndex }
 func (a nodeForceByPoint) Name(i int) int      { return int(a[i].pointIndex) }
